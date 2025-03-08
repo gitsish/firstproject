13745851d368b312cc0f25389f6f17458aaf13ef
gitsish <22501a4401@pvpsit.ac.in>
2025-03-08 15:58:17 +0530

    backend api updated

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/api/auth", auth);
+app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/cart",cartRoutes)
